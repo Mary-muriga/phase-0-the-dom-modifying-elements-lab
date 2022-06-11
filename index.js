8bff75b2991b5aa508c924ef9242d1cbd70cb2ea
Marymuriga <marymuriga@gmail.com>
2022-06-12 00:24:26 +0300

    commit tree 6deef56bf72fe3bc447f0ebd412e7ab3167061bd

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const newHeader = document.createElement('h1');
 
 newHeader.id = 'victory';
 
-newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.innerHTML = "Mary is the champion";
 
-
+document.body.appendChild(newHeader);
 
 
